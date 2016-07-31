89115b891f387071b6c89cd3d4d4d8afd947b100
Amr Alaa <n1amr1@gmail.com>
2016-07-31 10:20:09 +0200

    test beans

---
+++ src/com/n1amr/learn/jsf/FormBean.java
@@ -0,0 +1,42 @@
+package com.n1amr.learn.jsf;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
+import javax.persistence.Entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity(name = "form")
+@ManagedBean
+@SessionScoped
+public class FormBean implements Serializable {
+	private String message = "message from bean";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	private Collection<SelectItem> items = new ArrayList<SelectItem>();
+
+	public Collection<SelectItem> getItems() {
+		return items;
+	}
+
+	public void setItems(Collection<SelectItem> items) {
+		this.items = items;
+	}
+
+	@PostConstruct
+	public void init() {
+		items.add(new SelectItem("one"));
+		items.add(new SelectItem("two"));
+	}
+}
